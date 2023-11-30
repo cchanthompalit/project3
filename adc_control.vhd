7aee22a8789aa578a3146fe66ca14242e8d86756
jake2618 <149968969+jake2618@users.noreply.github.com>
2023-11-30 00:00:00 -0500

    Update adc_control.vhd

--- adc_control.vhd
+++ adc_control.vhd
@@ -1,69 +1,95 @@
 library ieee;
 use ieee.std_logic_1164.all;
 use ieee.numeric_std.all;
-use ieee.std_logic_unsigned.all;
+
+library work;
+use work.project_pkg.all;
 
 entity adc_control is
-	port (
-		clk_10MHz: in std_logic; 							-- 10 MHz clk into ADC
-		reset:	  in std_logic;							-- Reset
-		adc_data:  out std_logic_vector(11 downto 0);-- 12-bit ADC data
-		clk_out:	  out std_logic;
-		sent_data: out std_logic_vector(7 downto 0)
-);
-	
-end adc_control;
+    generic (
+        ADDR_WIDTH : natural := 6
+    );
+    port (
+        clock: in std_logic;
+        reset: in std_logic;
+        head_ptr: in natural range 0 to 2**ADDR_WIDTH - 1;
+        tail_ptr: buffer natural range 0 to 2**ADDR_WIDTH - 1
+    );
+end entity adc_control;
 
-architecture rtl of adc_control is
-	signal adc_soc: std_logic;								-- start of conversion signal
-	signal adc_tsen: std_logic;							-- temp sensing signal
-	signal adc_dout: natural range 0 to 2**12 - 1;  -- data output signal
-	signal adc_eoc: std_logic;								-- end of conversion signal
-	signal adc_clk_dft: std_logic;						-- divided clk signal
-	
-	signal adc_convert_done: std_logic;							-- signals that conversion is complete
-	signal adc_out: std_logic_vector(7 downto 0);			-- data obtained from ADC
-	
-	signal gray_input: std_logic;
-	signal synched_output: std_logic;
-	
-	
+architecture cons_fsm of adc_control is
+    
+    type state_type is
+        (Init, Start, Waiting, Reading, Increment);
+
+    signal state, next_state: state_type := Init;
+
+    function abs_difference(
+            a, b: in natural
+        ) return natural
+    is
+    begin
+        if a > b then
+            return a - b;
+        else
+            return b - a;
+        end if;
+    end function abs_difference;
+
+    function valid_ptrs(
+            head, tail: in natural
+        ) return boolean
+    is
+    begin
+        if tail < head then
+            return (head - tail) > 1;
+        elsif tail = 2**ADDR_WIDTH - 1 then
+            return head /= 0;
+        else
+            return true;
+        end if;
+    end function valid_ptrs;
 begin
-	
-	-- 2-stage synchronizer
-	synchronizer_inst: entity work.synchronizer
-		port map (
-			clk => adc_clk_dft,
-			rst => reset,
-			data_in => gray_input,
-			data_out => synched_output
-		);
-		
-	process (clk_10MHz, reset)
-	begin
-		if reset = '1' then
-			gray_input <= '0';
-		elsif rising_edge(clk_10MHz) then
-			gray_input <= adc_clk_dft;
-		end if;
-	end process;
-	
-	-- ADC
-	max10_adc_inst: entity work.max10_adc
-		port map (
-			pll_clk => clk_10MHz,
-			chsel => adc_chsel,					   -- channel select
-			soc => adc_soc,							-- start of conversion
-			tsen => adc_tsen,							-- temp sensing signal
-			dout => adc_dout,							-- data output
-			eoc => adc_eoc,							-- end of conversion
-			clk_dft => adc_clk_dft
-		);
-	
-	-- assign ADC data output
-	adc_data <= adc_out;
-	
-	
+    transition_function: process(state) is
+    begin
+        next_state <= state;
+        case (state) is
+            when Init => next_state <= Start;
+            when Start => next_state <= Waiting;
+            when Waiting =>
+                if valid_ptrs(head_ptr, tail_ptr) then
+                    next_state <= Increment;
+                end if;
+            when Increment => next_state <= Waiting;
+            when others => next_state <= Start;
+        end case;
+    end process transition_function;
 
-end rtl;
-		
+    save_state: process(clock) is
+    begin
+        if reset = '0' then
+            state <= Init;
+        elsif rising_edge(clock) then
+            state <= next_state;
+        end if;
+    end process save_state;
+
+    output_function: process(clock) is
+    begin
+        if reset = '0' then
+            tail_ptr <= 2**ADDR_WIDTH - 1;
+        elsif rising_edge(clock) then
+            if state = Init then
+                tail_ptr <= 2**ADDR_WIDTH - 1;
+            end if;
+
+            if state = Increment then
+                if tail_ptr >= 2**ADDR_WIDTH - 1 then
+                    tail_ptr <= 0;
+                else
+                    tail_ptr <= tail_ptr + 1;
+                end if;
+            end if;
+        end if;
+    end process output_function;
+end architecture cons_fsm;
